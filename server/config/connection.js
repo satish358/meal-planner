395499eec2b14e870f4d20456acfa910ef644a0e
satish358 <satishmankar745@gmail.com>
2024-07-12 12:26:25 +0530

    db name

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://db_user_admin:Zeus8x@cluster0.2fbjpff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://db_user_admin:Zeus8x@cluster0.2fbjpff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/k-meal-planner"
 );
 
 module.exports = mongoose.connection;
